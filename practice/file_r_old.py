13720485299c26c5f4de3ad462e5734e74dc04bb
Joonu Jo <metamath@gmail.com>
2023-07-23 22:11:14 +0900

    modify file read

--- practice/file_r_old.py
+++ practice/file_r_old.py
@@ -1,22 +1,23 @@
 print("파일을 모두 읽습니다.")
-with open('file_w.txt', 'r', encoding='utf-8') as f:
-    # f.seek(6) # ascii는 1 byte,  대부분 문자는 3byte
-    print( f.read() ) # 숫자는 한 글자(character)
+f = open('file_w.txt', 'r', encoding='utf-8')
+print( f.read() ) # 숫자는 한 글자(character)
+f.close()
 
 print("\n파일을 한줄씩 읽습니다.")
-with open('file_w.txt', 'r', encoding='utf-8') as f:
-    print( f.readline(), end='' )
-    print( f.readline(), end='' )
+f = open('file_w.txt', 'r', encoding='utf-8')
+print( f.readline(), end='' )
+print( f.readline(), end='' )
+f.close()
 
 print("\n파일을 for문을 통해 모두 읽습니다.")
-with open('file_w.txt', 'r', encoding='utf-8') as f:
-    for line in f:
-        print(line, end='')
-    print()
+f = open('file_w.txt', 'r', encoding='utf-8')
+for line in f:
+    print(line, end='')
+f.close()
 
 print("\n파일을 모두 읽어 리스트로 반환 받습니다.")
-with open('file_w.txt', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-    for line in lines:
-        print(line, end='')
-    print()
+f = open('file_w.txt', 'r', encoding='utf-8')
+lines = f.readlines()
+for line in lines:
+    print(line, end='')
+f.close()
